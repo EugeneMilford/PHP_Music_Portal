00bcc3f7511c0897ed5bb2d8643d2048ba24c64a
EugeneMilford <58213754+EugeneMilford@users.noreply.github.com>
2022-03-22 21:30:21 +0200

    Add files via upload

--- about.php
+++ about.php
@@ -195,5 +195,17 @@
 				</div>
 			</section>
             
+                        <section class="ds page_copyright section_padding_25">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<div class="divided-content bottommargin_5"> <span><a class="social-icon socicon-facebook" href="#" title="Facebook"></a></span> <span><a class="social-icon socicon-twitter" href="#" title="Twitter"></a></span> <span><a class="social-icon socicon-youtube" href="#" title="Youtube"></a></span>								<span><a class="social-icon socicon-google" href="#" title="Google"></a></span> </div>
+							<p class="small-text big-spacing">&copy; Copyright 2022. All Rights Reserved.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+                        
         </body>
         </html>
